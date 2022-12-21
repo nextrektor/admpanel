4c6e2c25cf3a991ad02b2c7d7aab66a986f79727
nale <93458378+nextrektor@users.noreply.github.com>
2022-12-21 09:51:08 +0700

    updating admin panel

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -141,7 +141,138 @@
                     <small class="text muted">Last 24 Hours</small>
                 </div>
             </div>
+
+            {{-- >>----------- RECENT ORDERS -----------<< --}}
+            <div class="recent-orders">
+                <h2>Recent Orders</h2>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Product Name</th>
+                            <th>Product Number</th>
+                            <th>Payment</th>
+                            <th>Status</th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Baju Teletubbies</td>
+                            <td>81197</td>
+                            <td>Due</td>
+                            <td class="warning">Pending</td>
+                            <td class="primary">Details</td>
+                        </tr>
+                    <tr>
+                            <td>Baju Teletubbies</td>
+                            <td>81197</td>
+                            <td>Due</td>
+                            <td class="warning">Pending</td>
+                            <td class="primary">Details</td>
+                        </tr>
+                    <tr>
+                            <td>Baju Teletubbies</td>
+                            <td>81197</td>
+                            <td>Due</td>
+                            <td class="warning">Pending</td>
+                            <td class="primary">Details</td>
+                        </tr>
+                    <tr>
+                            <td>Baju Teletubbies</td>
+                            <td>81197</td>
+                            <td>Due</td>
+                            <td class="warning">Pending</td>
+                            <td class="primary">Details</td>
+                        </tr>
+                    <tr>
+                            <td>Baju Teletubbies</td>
+                            <td>81197</td>
+                            <td>Due</td>
+                            <td class="warning">Pending</td>
+                            <td class="primary">Details</td>
+                        </tr>
+                    </tbody>
+                </table>
+                <a href="#">Show All</a>
+            </div>
         </main>
+
+        {{-- >>----------- BAGIAN KANAN -----------<< --}}
+        <div class="right">
+            <div class="top">
+                <button id="menu-btn">
+                    <span class="material-icons-sharp">menu</span>
+                </button>
+                <div class="theme-toggler">
+                    <span class="material-icons-sharp active">light_mode</span>
+                    <span class="material-icons-sharp">dark_mode</span>
+                </div>
+                <div class="profile">
+                    <div class="info">
+                        <p>Hey, <b>Marvina</b></p>
+                        <small class="text-muted">Admin</small>
+                    </div>
+                    <div class="profile-photo">
+                        <img src="{{ asset('img/profile-1.jpg') }}" alt="">
+                    </div>
+                </div>
+            </div>
+
+            {{-- >>----------- RECENT UPDATES -----------<< --}}
+            <div class="recent-updates">
+                <h2>Recent Updates</h2>
+                <div class="updates">
+                    <div class="update">
+                        <div class="profile-photo">
+                            <img src="{{ asset('img/profile-2.jpg') }}" alt="">
+                        </div>
+                        <div class="message">
+                            <p><b>Davina Theresa</b> received her order of
+                                Hati Marcell</p>
+                            <small class="text-muted">2 Minutes Ago</small>
+                        </div>
+                    </div>
+                    <div class="update">
+                        <div class="profile-photo">
+                            <img src="{{ asset('img/profile-2.jpg') }}" alt="">
+                        </div>
+                        <div class="message">
+                            <p><b>Davina Theresa</b> received her order of
+                                Hati Marcell</p>
+                            <small class="text-muted">2 Minutes Ago</small>
+                        </div>
+                    </div>
+                    <div class="update">
+                        <div class="profile-photo">
+                            <img src="{{ asset('img/profile-2.jpg') }}" alt="">
+                        </div>
+                        <div class="message">
+                            <p><b>Davina Theresa</b> received her order of
+                                Hati Marcell</p>
+                            <small class="text-muted">2 Minutes Ago</small>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            {{-- >>----------- SALES ANALYTICS -----------<< --}}
+            <div class="sales-analytics">
+                <h2>Sales Analytics</h2>
+                <div class="item online">
+                    <div class="icon">
+                        <span class="material-icons-sharp">shopping_cart</span>
+                    </div>
+                    <div class="right">
+                        <div class="info">
+                            <h3>ONLINE ORDERS</h3>
+                            <small class="text-muted">Last 24 Hours</small>
+                        </div>
+                        <h5 class="success">+39%</h5>
+                        <h3>3849</h3>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </body>
 
